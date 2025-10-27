78188638b807860f9053a030207f25b22e107790
pham-w <ha.pham@student.htw-berlin.de>
2025-10-27 14:04:40 +0100

    Bsp.daten erweitert

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,12 +4,22 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 
 const items= [
 {
-Name:"Lili",
-age:90
+name:"Lilie",
+age:90,
+geburtsdatum:"01.01.2000",
+favColor:"blau",
+hobby:"fußball",
+favFood:"pizza",
+dreamJob:"astronaut"
 },
 {
-Name:"Leonie",
-age:99
+name:"Liliane",
+age:99,
+geburtsdatum:"02.02.2002",
+favColor:"rot",
+hobby:"lesen",
+favFood:"pasta",
+dreamJob:"arzt"
 }
 ]
 </script>
@@ -19,17 +29,32 @@ age:99
         <thead>
         <tr>
           <th>Name</th>
-          <th>Age</th>
+          <th>Alter</th>
+          <th>Geburtsdatum</th>
+          <th>Lieblingsfarbe</th>
+          <th>Hobby</th>
+          <th>Lieblingsfarbe</th>
+          <th>Traumberuf</th>
         </tr>
         </thead>
         <tbody>
         <tr v-for="item in items" :key="item.id">
           <td>{{item.name}}</td>
           <td>{{item.age}}</td>
+          <td>{{item.geburtsdatum}}</td>
+          <td>{{item.favColor}}</td>
+          <td>{{item.hobby}}</td>
+          <td>{{item.favFood}}</td>
+          <td>{{item.dreamJob}}</td>
         </tr>
         <tr>
           <td>{{ nameField }}</td>
           <td>{{ ageField }}</td>
+          <td>{{ geburtsdatumField }}</td>
+          <td>{{ favColorField }}</td>
+          <td>{{ hobbyField }}</td>
+          <td>{{ favFoodField }}</td>
+          <td>{{ dreamJobField }}</td>
         </tr>
         </tbody>
       </table>
