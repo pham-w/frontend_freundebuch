a9d7f6dde278a414781246d3f76fc967207bef38
pham-w <ha.pham@student.htw-berlin.de>
2025-12-31 23:59:36 +0100

    homeView angepasst

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const profiles = ref([]);
-const loading = ref(true);
-const error = ref<string | null>(null);
-
-onMounted(async () => {
-  try {
-    const response = await fetch("https://webtech-projekt-1-p0l3.onrender.com/seite");
-    if (!response.ok) throw new Error("HTTP " + response.status);
-    profiles.value = await response.json();
-  } catch (e: any) {
-    error.value = e.message;
-  } finally {
-    loading.value = false;
-  }
-});
+import FriendBook from '@/components/FriendBook.vue'
 </script>
 
 <template>
-  <main style="padding:20px;">
-    <h1>API Test – Daten vom Backend</h1>
-
-    <p v-if="loading">Lade Daten…</p>
-    <p v-if="error">Fehler: {{ error }}</p>
-
-    <pre v-if="profiles.length">{{ profiles }}</pre>
-  </main>
+  <FriendBook />
 </template>
