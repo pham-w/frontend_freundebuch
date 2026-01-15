3779ce8b0f51de855504d3306818c6d7d4b46ba5
pham-w <ha.pham@student.htw-berlin.de>
2026-01-15 22:38:37 +0100

    API base zu render link angepasst

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE ?? "http://localhost:8080";
+const API_BASE = import.meta.env.VITE_API_BASE ?? "https://webtech-projekt-1-p0l3.onrender.com";
 
 export type AuthUser = { id: number; email: string; name: string };
 
